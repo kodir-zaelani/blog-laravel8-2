e345ba32cdf08c38f1e8f1adef2c84a4aa93c26e
kodir-zaelani <kodir.zaelani78@gmail.com>
2021-02-04 20:52:01 +0800

    update admin footer.blade.php

--- resources/views/admin/templates/partials/footer.blade.php
+++ resources/views/admin/templates/partials/footer.blade.php
@@ -2,8 +2,15 @@
     <b>V 1.0 | </b>
     <strong>Copyright &copy; 2020 - @php
         echo  date('Y');
-    @endphp |  <a href="http://{{ $global_settings->website }}">{{ $global_settings->title }}</a>.</strong>
-    All rights reserved.
+    @endphp |  
+     @if ($global_settings->website)
+     <a href="http://{{ $global_settings->website }}" target="_blank">{{ $global_settings->title }}</a>.</strong>
+     All rights reserved.
+    @else
+        <a href="http://lamankreasi.com" target="_blank">Laman Kreasi</a>.</strong>
+        All rights reserved.
+        
+    @endif
     <div class="float-right d-none d-sm-inline-block">
         <b>Developed : Laman Kreasi</b>
     </div>
